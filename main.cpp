b9454298b93258246927855f579fc4a0a530b3b4
Andrew2000-jS <andresarevalo0609@gmail.com>
2022-06-04 19:41:12 -0400

    [Add] addPerson method & readme.md file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,54 @@
-#include<iostream>;
+#include <iostream>
 using namespace std;
 
-int main() {
-    cout<<"Init";
-    
-    return 0;
+unsigned int ClientId = 0;
+
+struct ClientData
+{
+    unsigned int id;
+    char firstName[20];
+    char lastName[20];
+    unsigned int phoneNumber;
+};
+
+ClientData *clientData = new ClientData[50];
+
+unsigned int findClient(unsigned int id)
+{
+    for (int i = 0; i <= ClientId; i++)
+    {
+        if (clientData[i].id == id)
+        {
+            return clientData[i].id;
+        }
+    }
 }
 
+void AddPerson()
+{
+    cout << "Ingrese nombre del cliente: ";
+    cin >> clientData[ClientId].firstName;
+    cout << "Ingrese apellido del cliente: ";
+    cin >> clientData[ClientId].lastName;
+    cout << "Ingrese numero de telefono: ";
+    cin >> clientData[ClientId].phoneNumber;
+
+    clientData[ClientId].id = ClientId;
+    clientData[ClientId].id = ClientId++;
+}
+
+void showClient(unsigned int id)
+{
+    unsigned int index = findClient(id);
+    cout << "Nombre : " << clientData[index].firstName << endl;
+    cout << "Apellido : " << clientData[index].lastName << endl;
+    cout << "Telefono : " << clientData[index].phoneNumber << endl;
+}
+
+int main()
+{
+    AddPerson();
+    showClient(0);
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Hotel
+
+---
+
+Automated system (simple) for a hotel.
+
+This is a work assigned to finish the semester
+
+## Technologies
+
+- C++
+
+### Authors
+
+Andres Arevalo
+Carlos pimentel
